5b19d9caaf75076a99c80e3a6264c0273c70116b
Vaishnav Agarwal <147909405+Vaishnav00769@users.noreply.github.com>
2025-12-07 22:15:52 +0530

    Time: 15 ms (9.94%) | Memory: 26.5 MB (12.74%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<int,int> um;
         for(int i : nums) um[i]++;
         vector<int> ans;
-        for( auto pair : um)
+        for( auto &pair : um)
         {
             if(pair.second > nums.size()/3) ans.push_back(pair.first);
         }
